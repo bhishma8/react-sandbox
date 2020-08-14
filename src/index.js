e5553ace16960fe77d41314b55bd628585dabf92
Ishan Sarma <ishan.baban@gmail.com>
2020-08-14 23:25:01 +0530

    react-syntax

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-var React = require("react");
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
