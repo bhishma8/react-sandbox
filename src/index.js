f56b601434417c7b7acc6fd8fcd943c577a3cd72
Ishan Sarma <ishan.baban@gmail.com>
2020-08-14 23:29:07 +0530

    practise-jsx

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
+    <h1>My Favourite Foods </h1>
+    <ul>
+      <li>Biriyani </li>
+      <li>Chicken</li>
+      <li>Dosa</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
