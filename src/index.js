aa3ca6deba4da1d9d12ce08c234c4854d51e253f
Ishan Sarma <ishan.baban@gmail.com>
2020-08-15 01:48:20 +0530

    react-styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+const date = new Date();
+const n = date.getHours();
+let text;
+let customStyle;
+if (n > 0 && n < 12) {
+  text = "Good morning";
+  customStyle = {
+    color: "red"
+  };
+} else if (n > 12 && n < 18) {
+  text = "Good afternoon";
+  customStyle = {
+    color: "green"
+  };
+} else {
+  text = "Good evening";
+  customStyle = {
+    color: "blue"
+  };
+}
 
-const customStyle = {
-  color: "red",
-  fontSize: "20px"
-};
 ReactDOM.render(
   <div>
-    <h1 style={customStyle}>Hello World ! </h1>
+    <h1 style={customStyle}>{text} </h1>
   </div>,
   document.getElementById("root")
 );
